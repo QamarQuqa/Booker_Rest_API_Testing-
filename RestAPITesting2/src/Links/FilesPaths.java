60e055527270b867033f62465a51d568829acd66
YOUR_USERNAME <YOUR@EMAIL>
2021-05-03 22:38:07 +0300

    first version

---
+++ RestAPITesting2/src/Links/FilesPaths.java
@@ -0,0 +1,13 @@
+package Links;
+
+public class FilesPaths {
+
+    public static String CreatBookingJSONFile="./Data//PostData//CreateBooking.json";
+    public static String UpdateUserJSONFile="./Data//PutData//updateUser.json";
+    public static String TokenJSONFileV="./Data//PostData//ValidLogin.json";
+    public static String TokenJSONFileN="./Data//PostData//InvalidLogin.json";
+    public static String NewBookingFullReqJSONFile="./Data//PostData//NewBookingFullReq.json";
+    public static String NewBookingMissReqJSONFile="./Data//PostData//NewBookingMissReq.json";
+    public static String NewBookingEmptyReqJSONFile="./Data//PostData//NewBookingEmptyReq.json";
+
+}
